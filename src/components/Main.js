acc45e3c6751a4ae32c99f57b5835e59e98104bb
Damian Ludef <damiinho@gmail.com>
2023-05-03 19:00:03 +0200

    img

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,12 @@
+import LogoIMG from "../img/logo.png";
+
 const Main = () => {
-  return <main>Tutaj będzie główna część strony</main>;
+  return (
+    <main>
+      Tutaj będzie główna część strony
+      <img src={LogoIMG} alt="logo" />
+    </main>
+  );
 };
 
 export default Main;
